efb6227b5500eda2b658e52a23a9bc38edcbeeb4
Nithin1729S <sureshnithin1729@gmail.com>
2026-05-07 22:44:34 +0530

    update scripts

--- toggle_power_settings.sh
+++ toggle_power_settings.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 STATE_FILE="$HOME/.toggle_power_settings_state"
-NOISE_PID_FILE="$HOME/.noise_pid"
 
 strip_uint32() {
     echo "$1" | sed 's/^uint32 //'
@@ -15,6 +14,7 @@ if [ ! -f "$STATE_FILE" ]; then
         gsettings get org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type
         gsettings get org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout
         gsettings get org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout
+        powerprofilesctl get
     } > "$STATE_FILE"
 
     gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
@@ -24,14 +24,9 @@ if [ ! -f "$STATE_FILE" ]; then
     gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
     gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 0
 
-    notify-send "Top Performance Mode Activated"
+    powerprofilesctl set performance
 
-    (
-        while true; do
-            /usr/bin/play -n -c1 synth 3 whitenoise band -n 1 1 fade h 1 3 1 gain -10000
-        done
-    ) &
-    echo $! > "$NOISE_PID_FILE"
+    notify-send "Top Performance Mode Activated"
 
 else
     mapfile -t settings < "$STATE_FILE"
@@ -42,6 +37,7 @@ else
     ORIGINAL_BATTERY_TYPE="${settings[3]}"
     ORIGINAL_AC_TIMEOUT=$(strip_uint32 "${settings[4]}")
     ORIGINAL_BATTERY_TIMEOUT=$(strip_uint32 "${settings[5]}")
+    ORIGINAL_POWER_PROFILE="${settings[6]}"
 
     gsettings set org.gnome.settings-daemon.plugins.power idle-dim $ORIGINAL_IDLE_DIM
     gsettings set org.gnome.desktop.session idle-delay $ORIGINAL_IDLE_DELAY
@@ -50,10 +46,7 @@ else
     gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout $ORIGINAL_AC_TIMEOUT
     gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout $ORIGINAL_BATTERY_TIMEOUT
 
-    if [ -f "$NOISE_PID_FILE" ]; then
-        kill "$(cat "$NOISE_PID_FILE")"
-        rm "$NOISE_PID_FILE"
-    fi
+    powerprofilesctl set "$ORIGINAL_POWER_PROFILE"
 
     rm "$STATE_FILE"
 
